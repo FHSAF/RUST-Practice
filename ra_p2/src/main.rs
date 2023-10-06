52c3122422c9e41053916bb9a621a8d59867be4b
Feda Hussain Muzaffari <muzaffari.feda@Fedas-MacBook-Air.local>
2023-10-06 16:25:29 +0200

    Rust safety

--- ra_p2/src/main.rs
+++ ra_p2/src/main.rs
@@ -1,13 +1,46 @@
 // Dangling pointer
+/* 
 #[derive(Debug)]
 enum Cereal {
     Berley, Millet, Rice,
     Rye, Spelt, Wheat
 }
 
-fn main() {
+fn Dangling_pointer()
+{
     let mut Grains: Vec<Cereal> = vec![];
     Grains.push(Cereal::Rye);
     drop(Grains);
     println!("{:?}", Grains);
+}
+
+use std::thread;
+fn race_condition()
+{
+    let mut data = 100;
+    thread::spawn(|| { data = 500; });
+    thread::spawn(|| { data = 1000; });
+    println!("{}", data);
+}
+
+
+fn buffer_overflow()
+{
+    let fruite = vec!["apple", "banana", "seb"];
+    let buff_of = fruite[4];
+    assert_eq!(buff_of, "malta");
+}
+*/
+
+fn iterator_invalidation()
+{
+    let mut letters = vec!["a", "b", "c"];
+
+    for letter in letters {
+        println!("{}", letter);
+        letters.push(letter.clone());
+    }
+}
+fn main() {
+    iterator_invalidation();
 }
