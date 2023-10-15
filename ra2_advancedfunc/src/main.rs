5f2c310eb021df1e9cd8a9c3926c02553a4557ad
Noora <fedameh2018@gmail.com>
2023-10-15 14:21:39 +0200

    arrays covered

--- ra2_advancedfunc/src/main.rs
+++ ra2_advancedfunc/src/main.rs
@@ -1,3 +1,4 @@
+
 use std::ops::{ Add };
 use std::time::{ Duration };
 
@@ -8,6 +9,30 @@ fn life_time_add<'a, 'b>(i: &'a i32, j: &'b i32) -> i32
 
 fn generic_add<T: Add<Output = T>>(a: T, b: T) -> T {
     a + b
+}
+
+fn arrays()
+{
+    let one = [1, 2, 3];
+    let two: [u8; 3] = [1, 2, 3];
+    let blank1 = [0; 3];
+    let blank2: [u8; 3] = [0; 3];
+
+    let arr = [one, two, blank1, blank2];
+    for item in arr
+    {
+        print!("{:?}: ", item);
+        for i in item.iter()
+        {
+            print!("\t{} + 10 = {} ", i, i+10);
+        }
+        let mut sum = 0;
+        for j in 0..item.len() 
+        {
+            sum += item[j];
+        }
+        println!("\t((Σ{:?} = {})", item, sum);
+    }
 }
 
 fn main()
@@ -22,4 +47,5 @@ fn main()
     let b = 20;
     let c = life_time_add(&a, &b);
     println!("{}, {}, {:?}, c={}", floats, ints, durations, c);
+    arrays();
 }
